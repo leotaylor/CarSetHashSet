75d120cc4ab8f4b617734d65eac0a7b20ba68dc2
Leo Taylor <leotaylor723@gmail.com>
2018-09-10 21:12:07 -0500

    Hashsets, unionwith

--- CarSetHashSet/Program.cs
+++ CarSetHashSet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarSetHashSet
 {
@@ -6,7 +7,36 @@ namespace CarSetHashSet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<string> ShowRoom = new HashSet<string>();
+            ShowRoom.Add("Saab Viggen");
+            ShowRoom.Add("Subaru WRX");
+            ShowRoom.Add("Tesla Model S");
+            ShowRoom.Add("Saturn Ion");
+            ShowRoom.Add("Saturn Ion");
+
+            HashSet<string> UsedLot = new HashSet<string>();
+            UsedLot.Add("Audi A4");
+            UsedLot.Add("Honda Civic");
+
+            ShowRoom.UnionWith(UsedLot);
+
+            ShowRoom.Remove("Tesla Model S");
+
+            HashSet<string> Junkyard = new HashSet<string>()
+            {
+                "Saturn Ion",
+                "Volvo P-1800",
+                "Saab Viggen",
+                "Volkswagen Jetta",
+                "Kia Optima",
+                "Saab 9-3"
+            };
+
+            ShowRoom.UnionWith(Junkyard);
+            ShowRoom.Remove("Kia Optima");
+
+            Console.WriteLine($"I have {ShowRoom.Count} cars.");
+            Console.ReadLine();
         }
     }
 }
